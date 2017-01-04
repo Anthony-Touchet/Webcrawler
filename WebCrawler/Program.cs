eac11cc3fbb7fb34fa69d4d30f6489a56066c45a
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-01-04 13:50:56 -0600

    Program Skill Searching
    Program can now scrap my personal site for my skills.

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -24,19 +24,40 @@ namespace WebCrawler
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("What is the word you would like to search?:");
-            var input = Console.ReadLine();
-
             string pageContent = null;
             var myRequest = (HttpWebRequest)WebRequest.Create("https://anthony-touchet.github.io/");    //Asking for the site
             var myResponse = (HttpWebResponse)myRequest.GetResponse();                                  //Response to that request
 
-            using (var sr = new StreamReader(myResponse.GetResponseStream()))
+            using (var sr = new StreamReader(myResponse.GetResponseStream()))   //Get the page info
             {
                 pageContent = sr.ReadToEnd();
             }
 
-            Console.WriteLine(pageContent.Contains(input) ? "There" : "Not there");
+            StreamWriter webToTextWriter = new StreamWriter(Environment.CurrentDirectory + "\\WebInfo.txt");    //Save out info
+            webToTextWriter.WriteLine(pageContent);                                                             //Write info to a text file.
+            webToTextWriter.Close();
+
+            StreamReader webToTextReader = new StreamReader(Environment.CurrentDirectory + "\\WebInfo.txt");    //Read file.
+
+            //GET SKILLS
+
+            string line;    //Line that we are reading
+            List<string> skills = new List<string>();
+
+            while ((line = webToTextReader.ReadLine()) != null)
+            {
+                if (line.Contains("<li>") && line.Contains("</li>")) //These are the symbols we are using to find the info.
+                {
+                    string skill = line.Split('>')[1];  //Shrinking the lines
+                    skill = skill.Substring(0, skill.Length - 4);
+                    skills.Add(skill);
+                }
+            }
+
+            foreach (var s in skills)
+            {
+                Console.WriteLine(s);
+            }
 
             Console.ReadLine();
         }
