a5d5670b6b4cd3dc5c27505c2a833c2b2a707acf
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-01-09 09:01:50 -0600

    Style Cop Run

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -10,10 +10,10 @@
 namespace WebCrawler
 {
     using System;
-    using System.Net;
-    using System.IO;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Net;
     using System.Text;
     using System.Threading.Tasks;
 
@@ -21,46 +21,48 @@ namespace WebCrawler
     {
         public static void Main(string[] args)
         {
-            bool active = true;
-            while(active == true)
+            var active = true;
+            while (active == true)
             {
-                Console.WriteLine("What would you like?");                          //Get Input
+                Console.WriteLine("What would you like?");                          // Get Input
                 var option = Console.ReadLine();
                 Console.WriteLine();
 
-                string site = "https://anthony-touchet.github.io";                  //Site name
+                var site = "https://anthony-touchet.github.io";                  // Site name
                 string pageContent = null;
-                var myRequest = (HttpWebRequest)WebRequest.Create(site);            //Asking for the site
-                var myResponse = (HttpWebResponse)myRequest.GetResponse();          //Response to that request
+                var myRequest = (HttpWebRequest)WebRequest.Create(site);            // Asking for the site
+                var myResponse = (HttpWebResponse)myRequest.GetResponse();          // Response to that request
 
-                using (var sr = new StreamReader(myResponse.GetResponseStream()))   //Get the page info
+                // Get the page info
+                using (var sr = new StreamReader(myResponse.GetResponseStream()))   
                 {
                     pageContent = sr.ReadToEnd();
                 }
 
-                StreamWriter webToTextWriter = new StreamWriter(Environment.CurrentDirectory + "\\WebInfo.txt");    //Save out info
-                webToTextWriter.WriteLine(pageContent);                                                             //Write info to a text file.
+                StreamWriter webToTextWriter = new StreamWriter(Environment.CurrentDirectory + "\\WebInfo.txt");    // Save out info
+                webToTextWriter.WriteLine(pageContent);                                                             // Write info to a text file.
                 webToTextWriter.Close();
 
-                StreamReader webToTextReader = new StreamReader(Environment.CurrentDirectory + "\\WebInfo.txt");    //Read file.
-                string line = null;     //Line that we are reading
-
-                switch (option)         //Choices
+                StreamReader webToTextReader = new StreamReader(Environment.CurrentDirectory + "\\WebInfo.txt");    // Read file.
+                string line = null;     // Line that we are reading
+                
+                // Choices
+                switch (option)         
                 {
                     case "Skills":
-                        FindSkills(line, webToTextReader);      //Prints out skills
+                        FindSkills(line, webToTextReader);      // Prints out skills
                         break;
 
                     case "Links":
-                        FindLinks(line, webToTextReader, site); //Prints all valid linkc on the page
+                        FindLinks(line, webToTextReader, site); // Prints all valid linkc on the page
                         break;
 
                     case "Quit":
-                        active = false;                         //Quit Program
+                        active = false;                         // Quit Program
                         break;
 
                     default:
-                        Console.WriteLine("Invalid Input");     //Not Valid Input
+                        Console.WriteLine("Invalid Input");     // Not Valid Input
                         break;
                 }
                 webToTextReader.Close();
@@ -69,18 +71,21 @@ namespace WebCrawler
             
         }
         
-        static void FindSkills(string line, StreamReader sr)
+        public static void FindSkills(string line, StreamReader sr)
         {
             List<string> skills = new List<string>();
 
             while ((line = sr.ReadLine()) != null)
             {
-                if (line.Contains("<li>") && line.Contains("</li>")) //These are the symbols we are using to find the info.
+                // These are the symbols we are using to find the info.
+                if (!line.Contains("<li>") || !line.Contains("</li>"))
                 {
-                    string skill = line.Split('>')[1];              //Spliting lines from tabs
-                    skill = skill.Substring(0, skill.Length - 4);   //Taking the back part off
-                    skills.Add(skill);
+                    continue;
                 }
+
+                string skill = line.Split('>')[1];              // Spliting lines from tabs
+                skill = skill.Substring(0, skill.Length - 4);   // Taking the back part off
+                skills.Add(skill);
             }
 
             foreach (var s in skills)
@@ -89,45 +94,49 @@ namespace WebCrawler
             }
         }
 
-        static void FindLinks(string line, StreamReader sr, string siteName)
+        public static void FindLinks(string line, StreamReader sr, string siteName)
         {
-            List<string> links = new List<string>();    //List of links
-
-            while ((line = sr.ReadLine()) != null)  //Read each line
+            List<string> links = new List<string>();    // List of links
+            
+            // Read each line
+            while ((line = sr.ReadLine()) != null)  
             {
-                if (line.Contains("<a href") && line.Contains("</a>")) //These are the symbols we are using to find the info.
+                // These are the symbols we are using to find the info.
+                if (!line.Contains("<a href") || !line.Contains("</a>"))
                 {
-                    string link = line; //Get Line
-                    
-                    //If Full link
-                    if (link.Contains("https://www"))
-                    {
-                        link = link.Split('"')[1];
-                        link = link.Substring(0, link.Length - 2);
-                        links.Add(link);
-                    }
+                    continue;
+                }
 
-                    //Else if other part of the site
-                    else
-                    {   
-                        //Create Proper Link
-                        link = link.Split('"')[1];
-                        link = siteName + "/" + link.Substring(0, link.Length);
+                string link = line; // Get Line
+
+                // If Full link
+                if (link.Contains("https://www"))
+                {
+                    link = link.Split('"')[1];
+                    link = link.Substring(0, link.Length - 2);
+                    links.Add(link);
+                }
+
+                // Else if other part of the site
+                else
+                {   
+                    // Create Proper Link
+                    link = link.Split('"')[1];
+                    link = siteName + "/" + link.Substring(0, link.Length);
                         
-                        //Try to get link
-                        try
-                        {
-                            var myRequest = (HttpWebRequest)WebRequest.Create(link);    //Asking for the site
-                            myRequest.Method = "HEAD";
-                            var myResponse = (HttpWebResponse)myRequest.GetResponse();  //Response to that request
-                            myResponse.Close();
-                            links.Add(link);        //If no errors happen, Link was good
-                        }
-                        catch
-                        {
-                            //Else the link doesn't exist
-                        }                       
+                    // Try to get link
+                    try
+                    {
+                        var myRequest = (HttpWebRequest)WebRequest.Create(link);    // Asking for the site
+                        myRequest.Method = "HEAD";
+                        var myResponse = (HttpWebResponse)myRequest.GetResponse();  // Response to that request
+                        myResponse.Close();
+                        links.Add(link);        // If no errors happen, Link was good
                     }
+                    catch
+                    {
+                        // Else the link doesn't exist
+                    }                       
                 }
             }
 
