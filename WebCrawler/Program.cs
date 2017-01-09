18119d9d026502db86b4594603de7006245e26fb
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-01-09 15:15:12 -0600

    More Genaric . Trying to Save

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -14,66 +14,73 @@ namespace WebCrawler
     using System.IO;
     using System.Linq;
     using System.Net;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class Program
     {
+        private static string _folderName = null;
         public static void Main(string[] args)
         {
+            Console.WriteLine("Enter a site:");                          // Get Input
+            var site = Console.ReadLine();
+            Console.WriteLine();
+          
             var active = true;
-            while (active == true)
+            while (active)
             {
                 Console.WriteLine("What would you like?");                          // Get Input
                 var option = Console.ReadLine();
                 Console.WriteLine();
 
-                var site = "https://anthony-touchet.github.io";                  // Site name
-                string pageContent = null;
-                var myRequest = (HttpWebRequest)WebRequest.Create(site);            // Asking for the site
-                var myResponse = (HttpWebResponse)myRequest.GetResponse();          // Response to that request
-
-                // Get the page info
-                using (var sr = new StreamReader(myResponse.GetResponseStream()))   
-                {
-                    pageContent = sr.ReadToEnd();
-                }
-
-                StreamWriter webToTextWriter = new StreamWriter(Environment.CurrentDirectory + "\\WebInfo.txt");    // Save out info
-                webToTextWriter.WriteLine(pageContent);                                                             // Write info to a text file.
-                webToTextWriter.Close();
-
-                StreamReader webToTextReader = new StreamReader(Environment.CurrentDirectory + "\\WebInfo.txt");    // Read file.
                 string line = null;     // Line that we are reading
                 
                 // Choices
                 switch (option)         
                 {
                     case "Skills":
-                        FindSkills(line, webToTextReader);      // Prints out skills
+                        FindSkills(line);      // Prints out skills
                         break;
 
-                    case "Links":
-                        FindLinks(line, webToTextReader, site); // Prints all valid linkc on the page
+                    case "Page Links":
+                        var validLinks = FindLinksOnPage(line, site); // Prints all valid linkc on the page
+                        foreach (var s in validLinks)
+                        {
+                            Console.WriteLine(s);
+                        }
+
+                        break;
+
+                    case "All Links":
+                        var fullLinks = FindLinksOnFullSite(line, site); // Prints all valid linkc on the page
+                        foreach (var s in fullLinks)
+                        {
+                            Console.WriteLine(s);
+                        }
+
                         break;
 
                     case "Quit":
                         active = false;                         // Quit Program
                         break;
 
+                    case "New Site":
+                        Console.WriteLine("Enter a site:");                          // Get Input
+                        site = Console.ReadLine();
+                        Console.WriteLine();
+                        break;
+
                     default:
                         Console.WriteLine("Invalid Input");     // Not Valid Input
                         break;
                 }
-                webToTextReader.Close();
+
                 Console.WriteLine();
-            }
-            
+            }    
         }
         
-        public static void FindSkills(string line, StreamReader sr)
+        public static void FindSkills(string line)
         {
-            List<string> skills = new List<string>();
+            var skills = new List<string>();
+            var sr = GetPageInfo("https://anthony-touchet.github.io");                  // Site name);
 
             while ((line = sr.ReadLine()) != null)
             {
@@ -83,66 +90,163 @@ namespace WebCrawler
                     continue;
                 }
 
-                string skill = line.Split('>')[1];              // Spliting lines from tabs
+                var skill = line.Split('>')[1];              // Spliting lines from tabs
                 skill = skill.Substring(0, skill.Length - 4);   // Taking the back part off
                 skills.Add(skill);
             }
 
+            sr.Close();
             foreach (var s in skills)
             {
                 Console.WriteLine(s);
             }
         }
 
-        public static void FindLinks(string line, StreamReader sr, string siteName)
+        public static List<string> FindLinksOnPage(string line, string siteName)
         {
-            List<string> links = new List<string>();    // List of links
-            
+            var links = new List<string>();    // List of links
+
+            var sr = GetPageInfo(siteName);                  // Site name);
+
             // Read each line
-            while ((line = sr.ReadLine()) != null)  
+            while ((line = sr.ReadLine()) != null)
             {
                 // These are the symbols we are using to find the info.
-                if (!line.Contains("<a href") || !line.Contains("</a>"))
+                if (!line.Contains("<a ") && !line.Contains("< a "))
                 {
                     continue;
                 }
 
-                string link = line; // Get Line
+                var link = line; // Get Line
 
                 // If Full link
-                if (link.Contains("https://www"))
+                if (link.Contains("https://") || link.Contains("www."))
                 {
-                    link = link.Split('"')[1];
-                    link = link.Substring(0, link.Length - 2);
-                    links.Add(link);
+                    var fullLinkStringList = link.Split('"');
+                    foreach (var s in fullLinkStringList)
+                    {
+                        if (s.Contains("https://") || link.Contains("www."))
+                        {
+                            link = s;
+                        }
+                    }
+
+                    // Try to get link
+                    if (IsSiteValid(link))
+                    {
+                        links.Add(link);
+                    }
                 }
 
+                // ReSharper disable once StyleCop.SA1108
                 // Else if other part of the site
                 else
-                {   
+                {
                     // Create Proper Link
-                    link = link.Split('"')[1];
-                    link = siteName + "/" + link.Substring(0, link.Length);
-                        
-                    // Try to get link
-                    try
+                    var partialLinkStringList = link.Split('"');
+                    for (int i = 0; i < partialLinkStringList.Length; i++)
                     {
-                        var myRequest = (HttpWebRequest)WebRequest.Create(link);    // Asking for the site
-                        myRequest.Method = "HEAD";
-                        var myResponse = (HttpWebResponse)myRequest.GetResponse();  // Response to that request
-                        myResponse.Close();
-                        links.Add(link);        // If no errors happen, Link was good
+                        if (partialLinkStringList[i].Contains("href"))
+                        {
+                            link = partialLinkStringList[i + 1];
+                        }
                     }
-                    catch
+                   
+                    if (!link.Contains(siteName))
                     {
-                        // Else the link doesn't exist
+                        link = siteName + link.Substring(0, link.Length);
+                    }
+
+                    // Try to get link
+                    if (IsSiteValid(link))
+                    {
+                        links.Add(link);
                     }                       
                 }
             }
 
-            foreach (var s in links)
+            sr.Close();
+            return links;
+        }
+
+        public static List<string> FindLinksOnFullSite(string line, string siteName)
+        {
+            var links = FindLinksOnPage(line, siteName);    // List of links
+            var tempLinks = links.ToList();                     // Create Temp list
+
+            // Weed out Outside sites
+            foreach (var l in tempLinks)
             {
-                Console.WriteLine(s);
+                if (l.Contains(siteName))
+                {
+                    continue;
+                }
+
+                links.Remove(l);
+            }
+
+            tempLinks = links.ToList();
+             
+            foreach (var l in tempLinks)
+            {
+                var pageReader = GetPageInfo(l);                  // Site name
+                pageReader.Close();
+                var newPageLinks = FindLinksOnPage(line, l);
+                
+                links.AddRange(newPageLinks);
+            }
+
+            return links;
+        }
+
+        public static StreamReader GetPageInfo(string site)
+        {
+            string pageContent;
+            var myRequest = (HttpWebRequest)WebRequest.Create(site);            // Asking for the site
+            var myResponse = (HttpWebResponse)myRequest.GetResponse();          // Response to that request
+
+            // Get the page info
+            using (var sr = new StreamReader(myResponse.GetResponseStream()))
+            {
+                pageContent = sr.ReadToEnd();
+            }
+ 
+            var siteArray = site.Split('/');
+            foreach (var s in siteArray)
+            {
+                if (s == "" || s == "https:" || s == "http:") continue;
+                _folderName = s.Split('.')[0];
+                break;
+            }
+
+            Directory.CreateDirectory(_folderName);
+
+            // Writing to Text File
+            var webToTextWriter = new StreamWriter(Environment.CurrentDirectory + "\\" + _folderName + "\\Website.txt");     // Save out info
+            webToTextWriter.WriteLine(pageContent);                                                                         // Write info to a text file.
+            webToTextWriter.Close();
+
+            var webInfoWriter = new StreamWriter(Environment.CurrentDirectory + "\\" + _folderName + "\\WebInfo.txt");       // Save out info
+            webInfoWriter.Close();
+
+            var webToTextReader = new StreamReader(Environment.CurrentDirectory + "\\" + _folderName + "\\Website.txt");     // Read file.
+            return webToTextReader;
+        }
+
+        public static bool IsSiteValid(string siteName)
+        {
+            try
+            {
+                var myRequest = (HttpWebRequest)WebRequest.Create(siteName);    // Asking for the site
+                myRequest.Method = "HEAD";
+                var myResponse = (HttpWebResponse)myRequest.GetResponse();  // Response to that request
+                myResponse.Close();
+                return true;
+            }
+            catch
+            {
+                // Else the link doesn't exist
+                return false;
             }
         }
     }
